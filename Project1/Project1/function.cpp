fc411a4bbe1e1f12ca3279e983e20c8f166e0ce2
Max <mmaksimm921@gmail.com>
2019-03-13 16:36:27 +0200

    class function

--- Project1/Project1/function.cpp
+++ Project1/Project1/function.cpp
@@ -1,6 +1,9 @@
 #include "Header.h"
 
 void Country::country_get_info(string str, int num) {
+	int position = str.find(",");
+	name = str.substr(0, position);
+	str = str.substr(position + 1, str.length());
 
 
 
--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -5,11 +5,36 @@ int main() {
 	if (!fileEuro)
 		cout << " File doesn`t open ";
 
-	string number;
+	string number, str;
 	getline(fileEuro, number);
 	int num = stoi(number);
 
-	cout << num;
+	Country ukr(2);
+
+	for (int i = 0; i < 2; i++) {
+		getline(fileEuro, str);
+		int position = str.find(",");
+		ukr.name = str.substr(0, position);
+		str = str.substr(position + 1, str.length());
+
+		for (int i = 0; i < num; i++) {
+			position = str.find(",");
+			string mark = str.substr(0, position);
+			ukr.mark[i] = stoi(mark);
+			str = str.substr(position + 1, str.length());
+		}
+
+	}
+	
+	for (int i = 0; i < 2; i++)
+		cout << ukr.name << endl;
+	for (int i = 0; i < 2; i++) {
+		for (int i = 0; i < num; i++)
+			cout << ukr.mark[i] << " ";
+
+		cout << endl << endl;
+	}
+
 
 
 
